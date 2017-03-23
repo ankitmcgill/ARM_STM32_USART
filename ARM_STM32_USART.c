b396b7a26933a2701e1935d531034a6fa31254f1
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-03-23 21:56:07 +0530

    intitial file commit & added gitognore file

---
+++ ARM_STM32_USART.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+* STM32 USART LIBRARY (SPL WRAPPER)
+*
+* MARCH 23 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+*		(1) www.edn.com/electronics-blogs/embedded-basics/4440395/USART-vs-UART--Know-the-difference
+* ***********************************************************************/
+
+#include "ARM_STM32_USART.h"
---
+++ ARM_STM32_USART.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+* STM32 USART LIBRARY (SPL WRAPPER)
+*
+* MARCH 23 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+*		(1) www.edn.com/electronics-blogs/embedded-basics/4440395/USART-vs-UART--Know-the-difference
+* ***********************************************************************/
+
+#ifndef _ARM_STM32_USART_
+#define _ARM_STM32_USART_
+
+#endif
+
